738a7e38204c1695e14d8b77ff193c1207c67b16
ZiyadPC <zidr2010@icloud.com>
2023-11-08 23:59:00 +0300

    BE 2 Excercise 4

--- login.js
+++ login.js
@@ -4,29 +4,43 @@ class CustomEventEmitter extends EventEmitter {}
 
 const eventEmitter = new CustomEventEmitter();
 
-function simulateLogin(user) {
-  const randomDelay = Math.random() * 1.9 + 0.1;
+function simulateLogin(username, password) {
+  const randomDelay = Math.random() * 1.9 + 0.1; // Random delay between 0.1 and 2 seconds
   setTimeout(() => {
     const timestamp = new Date().toLocaleString();
-    console.log(`${timestamp}: ${user} logged in`);
-    eventEmitter.emit('userLoggedIn', user);
+    if (username === 'admin' && password === 'password') {
+      console.log(`${timestamp}: Successful login for ${username}`);
+      eventEmitter.emit('userLoggedIn', username);
+    } else {
+      console.log(`${timestamp}: Failed login attempt for ${username}`);
+      eventEmitter.emit('userLoginFailed', username);
+    }
   }, randomDelay * 1000);
 }
 
-eventEmitter.on('userLoggedIn', (user) => {
-  console.log(`Welcome, ${user}!`);
+eventEmitter.on('userLoggedIn', (username) => {
+  console.log(`Welcome, ${username}!`);
 });
 
-eventEmitter.on('userLoggedOut', (user) => {
-  console.log(`Goodbye, ${user}!`);
+eventEmitter.on('userLoginFailed', (username) => {
+  console.log(`Login failed for ${username}`);
 });
 
-// Simulate user logins
-simulateLogin('User1');
-simulateLogin('User2');
-simulateLogin('User3');
+function promptUser() {
+  process.stdout.write('Enter username (or "exit" to quit): ');
 
-const logoutDelay = Math.random() * 1.9 + 0.1; 
-setTimeout(() => {
-  eventEmitter.emit('userLoggedOut', 'User2');
-}, logoutDelay * 1000);
+  process.stdin.once('data', (data) => {
+    const userInput = data.toString().trim();
+
+    if (userInput.toLowerCase() === 'exit') {
+      process.exit();
+    } else {
+      const password = Math.random() < 0.5 ? 'password' : 'wrongpassword'; 
+      simulateLogin(userInput, password);
+      promptUser();
+    }
+  });
+}
+
+console.log('Simulating user logins...');
+promptUser();
